f39463e80d3c065024a96fb60bb7eab1e726fb2c
adnanq2004 <aquayyum20@stuy.edu>
2020-10-21 15:40:28 -0400

    I re-tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
 public class Demo {
 
 	public static void printLoop(int x) {
-		for (int i = 0; i <= x; i++) {
-			for (int j = 0; j <= i; j++) {
+		for (int i = 1; i <= x; i++) {
+			for (int j = 0; j < x - i + 1; j++) {
 				System.out.print(i);
 			}
 			System.out.println("");
@@ -10,7 +10,7 @@ public class Demo {
 	}
 
 	public static void main(String[] args){
-		printLoop(10);
+		printLoop(Integer.valueOf(args[0]));
 	}
 
 }
