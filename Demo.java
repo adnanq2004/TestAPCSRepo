0555d7915bfaf9bcf1b03745f053c835f9772cd5
adnanq2004 <aquayyum20@stuy.edu>
2020-10-21 20:00:40 -0400

    added arrToString

--- Demo.java
+++ Demo.java
@@ -1,5 +1,22 @@
 public class Demo {
 
+	public static String arrToString(int[] arr) {
+		String val = "{";
+		for (int i = 0; i < arr.length; i++) {
+			val += Integer.toString(arr[i]);
+			if (i!- arr.length(-1)) {
+				val += ", "
+			}
+			else {
+				val += "}";
+			}
+		}
+		if (arr.length == 0) {
+			val == "{}";
+		}
+		return val;
+	}
+
 	public static void printLoop(int x) {
 		for (int i = 1; i <= x; i++) {
 			for (int j = 0; j < x - i + 1; j++) {
