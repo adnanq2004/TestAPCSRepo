8dee7752d601607edcc68f96a44d47107682f76b
adnanq2004 <aquayyum20@stuy.edu>
2020-10-21 22:12:17 -0400

    added create2DArraysRandomized, and made main function only do

--- Demo.java
+++ Demo.java
@@ -39,12 +39,23 @@ public class Demo {
 			for (int col = 0; col < cols; col++) {
 				r = random.nextInt(maxValue + 1);
 				thing[row][col] = r;
-				//if (count == 4) {
-				//
-				//}
 			}
 		}
 		return thing;
+	}
+
+	public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue) {
+		int[][] otherthing = new int[rows][];
+		Random random = new Random();
+		for (int row = 0; row < rows; row++) {
+			int r = random.nextInt(cols+1);
+			otherthing[row] = new int[r];
+			for (int col = 0; col < r; col ++) {
+				int n = random.nextInt(maxValue + 1);
+				otherthing[row][col] = n;
+			}
+		}
+		return otherthing;
 	}
 
 	public static void printLoop(int x) {
@@ -57,11 +68,13 @@ public class Demo {
 	}
 
 	public static void main(String[] args){
-		//printLoop(Integer.valueOf(args[0]));
+		printLoop(Integer.valueOf(args[0]));
 		//int[][] thing = new int[Integer.valueOf(args[0])][Integer.valueOf(args[1])];
 		//System.out.println(arrayDeepToString(thing));
-		String returnval = arrayDeepToString(create2DArray(Integer.valueOf(args[0]), Integer.valueOf(args[1]), Integer.valueOf(args[2])));
-		System.out.println(returnval);
+		//String returnval = arrayDeepToString(create2DArray(Integer.valueOf(args[0]), Integer.valueOf(args[1]), Integer.valueOf(args[2])));
+		//System.out.println(returnval);
+		//String val = arrayDeepToString(create2DArrayRandomized(Integer.valueOf(args[0]), Integer.valueOf(args[1]), Integer.valueOf(args[2])));
+		//System.out.println(val);
 	}
 
 }
